ad8d394b6d6d6b81481c5a51deedc45dbc0cf6cf
Omar <robodude20000@gmail.com>
2025-10-31 14:21:45 +0000

    Finished level 1 with a pylint score of 9.75. Passed all tests

--- Level-1/task_one.py
+++ Level-1/task_one.py
@@ -1,4 +1,49 @@
+"""Define Trainee and Assessment objects"""
 from datetime import date
+
+class Trainee:
+    """Define trainee object with name email and date of birth (DOB)"""
+    def __init__(self, name: str, email: str, date_of_birth: date):
+        self.name = name
+        self.email = email
+        self.date_of_birth = date_of_birth
+        self.assessments = []
+
+    def get_age(self):
+        """Return trainee age based on DOB"""
+        today = date.today()
+        age = today.year - self.date_of_birth.year
+        if (today.month, today.day) < (self.date_of_birth.month, self.date_of_birth.day):
+            age -= 1
+        return age
+
+    def add_assessment(self, assessment: Assessment):
+        """Add an assessment object to trainee's assessment list"""
+        self.assessments.append(assessment)
+
+    def get_assessment(self, name: str) -> Assessment:
+        """Get an assessment from trainee's assessment list based on assessment name"""
+        for assessment in self.assessments:
+            if assessment.name == name:
+                return assessment
+        return None
+
+class Assessment:
+    """Define assessment class with name, type and score"""
+    assessment_types = ["multiple-choice", "technical", "presentation"]
+
+    def __init__(self, name: str, assessment_type: str, score: float):
+        self.name = name
+
+        if assessment_type not in self.assessment_types:
+            raise ValueError()
+
+        self.assessment_type = type
+
+        if not 0 <= score <= 100:
+            raise ValueError()
+
+        self.score = score
 
 
 
